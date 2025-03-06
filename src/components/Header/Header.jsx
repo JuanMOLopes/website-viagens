dc481a592e484d5e87cf7fccbca6b59ae6a8be1f
JuanMOLopes <juan.oliiveira2007@gmail.com>
2025-03-06 14:47:25 -0300

    navbar

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,12 +1,14 @@
 import "./Header.css";
 import { Link } from "react-router-dom";
 
+import Logo from "../../../public/logo.png"
+
 function Header() {
   return (
     <>
       <header>
         <Link to="/">
-          <img src="" alt="" />
+          <img src={Logo} alt="" className="logo"/>
         </Link>
       </header>
     </>
