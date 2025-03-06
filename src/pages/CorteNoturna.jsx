0272a3016607b048b1d5e6926b61dcf9115e1421
MARIN-Zenin <marin.lucas009@gmail.com>
2025-03-06 14:51:46 -0300

    add ultima descrição

--- src/pages/CorteNoturna.jsx
+++ src/pages/CorteNoturna.jsx
@@ -11,7 +11,7 @@ function CorteNoturna() {
 
                 <img src="" alt="" />
 
-                <p>descrição</p>
+                <p>Bem-vindos ao Reino de Corte Noturna, um lugar onde a magia da noite reina. Exploraremos florestas sombrias, castelos imponentes e campos iluminados pela lua, habitados por criaturas misteriosas e seres mágicos. Cada canto do reino esconde segredos, e sob o brilho das estrelas, a escuridão se torna um espetáculo encantado. Prepare-se para viver uma experiência única, onde o mistério e a beleza se encontram a cada passo.</p>
 
                 <p>valor</p>
 
