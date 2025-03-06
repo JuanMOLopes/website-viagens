a731e8a33ee7f79d43ab457da71ed50e1e146bf0
MARIN-Zenin <marin.lucas009@gmail.com>
2025-03-06 14:44:50 -0300

    descrições

--- src/pages/GravityFalls.jsx
+++ src/pages/GravityFalls.jsx
@@ -2,7 +2,7 @@ import Header from "../components/Header/Header"
 import Estrelas from "../components/Avaliação/Estrelas"
 import Navbar from "../components/Navbar/Navbar"
 import "../App.css";
-import "../App.css";
+
 
 function GravityFalls() {
     return (
@@ -12,7 +12,7 @@ function GravityFalls() {
 
                 <img src="" alt="" />
 
-                <p>descrição</p>
+                <p>Gravity Falls é uma cidade pequena e misteriosa localizada no Oregon. Apesar de parecer tranquila, é repleta de fenômenos sobrenaturais e estranhos, como criaturas e eventos inexplicáveis. O local é conhecido por atrações turísticas peculiares, como a Mystery Shack, uma loja de curiosidades que esconde segredos ainda maiores. É uma cidade onde o comum e o extraordinário se misturam o tempo todo.</p>
 
                 <p>valor</p>
 
