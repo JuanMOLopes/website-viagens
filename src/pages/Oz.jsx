b4499adff95c18a036322170c6812d9d8de2c439
JuanMOLopes <juan.oliiveira2007@gmail.com>
2025-03-12 07:51:18 -0300

    banners

--- src/pages/Oz.jsx
+++ src/pages/Oz.jsx
@@ -1,25 +1,24 @@
-import Header from "../components/Header/Header"
-import Estrelas from "../components/Avaliação/Estrelas"
-import Navbar from "../components/Navbar/Navbar"
 import "../App.css";
-import Ozz from "../../public/ozzzzz.png"
 
+import Header from "../components/Header/Header"
+import Navbar from "../components/Navbar/Navbar"
+import Estrelas from "../components/Avaliação/Estrelas"
+import Footer from "../components/Footer/Footer"
+
+import Oz2 from "../../public/oz2.png"
 
 function Oz() {
     return (
         <>
             <div>
                 <Header />
-
-                <img src={Ozz} alt="" />
-
-                <p>Bem-vindo a Oz, um destino mágico onde a aventura e a fantasia ganham vida! Explore a vibrante Cidade das Esmeraldas, com sua arquitetura brilhante e o imponente castelo do Mágico, ou siga a icônica Estrada de Tijolos Amarelos até vilarejos encantadores como Munchkinlândia. Descubra as paisagens exuberantes da Terra dos Bons e os mistérios sombrios do Oeste, onde bruxas poderosas habitam. Com habitantes peculiares, criaturas fantásticas e surpresas a cada curva, Oz é o lugar perfeito para quem busca magia, emoção e um toque de sonho em sua jornada!</p>
-
-                <p>valor</p>
-
-                <Estrelas />
-
                 <Navbar />
+
+                <div className="container">
+                    <img src="" alt="" />
+                </div>
+
+                <Footer />
             </div>
         </>
     )
